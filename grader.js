ea34fd75bfb057c1d3d3e59d222e7e2b1fb1d206
Patrick Ryan <secretengineer@gmail.com>
2013-07-10 15:56:07 +0000

    fixed typo in options

--- grader.js
+++ grader.js
@@ -66,7 +66,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url<url>', 'url of index.html',)
+        .option('-u, --url <url>', 'url of index.html')
         .parse(process.argv);
       if(program.url) {
         restler.get(program.url).on('complete', function(result, response) {
