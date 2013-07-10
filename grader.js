a709d5e7003e9036526f8982974ad17e011bb649
Patrick Ryan <secretengineer@gmail.com>
2013-07-10 15:48:25 +0000

    added url or file input checking

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,10 +66,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url<url>', 'url of index.html',)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+      if(program.url) {
+        restler.get(program.url).on('complete', function(result, response) {
+          if (result instanceof Error) {
+            console.log("Error with URL");
+            process.exit(1);
+          } else {
+            run(result;)
+          }
+        }
+      } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+      } 
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
